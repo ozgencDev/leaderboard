e677fe1fc397069b173abbed0fe95eb9e43a01dc
ozgencdev <ozgencdev@gmail.com>
2022-01-25 21:06:23 +0300

    add-edit: getTopUsers binds mongoose data with redis

---
+++ controller/api.controller.js
@@ -0,0 +1,16 @@
+const UserModel = require("../models/user.model");
+const LeaderBoardModel = require("../models/board.model");
+
+const boardModel = new LeaderBoardModel();
+
+exports.getTopUsers = async (req, res) => {
+  const id = req.params.id;
+  const query = await UserModel.getIdAndValue();
+  boardModel.addUsersForLeaderboard(query);
+
+  const leaderboard = await boardModel.getTopUsers(100);
+  const around = await boardModel.getArounds(id);
+  const concatted = leaderboard.concat(around);
+
+  res.send(concatted);
+};
