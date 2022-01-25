52a2af15b4ad64e2826323cca6804fd2cf61eb09
ozgencdev <ozgencdev@gmail.com>
2022-01-25 21:09:22 +0300

    edit: abstracts from connection logic

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { connectRedis } = require("./config/redis.config");
+const connectMongo = require("./config/mongo.config");
+const apiRouter = require("./router/api.route");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
+connectMongo();
+connectRedis();
+
+app.use("/api", apiRouter);
 
 app.listen(3000, function () {
   console.log("Server listening port 3000");
