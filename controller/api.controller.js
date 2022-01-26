42e6b76b097fe10ea51be79a32bfe57a31257830
ozgencdev <ozgencdev@gmail.com>
2022-01-26 18:45:42 +0300

    edit: clean getTopUsers and reLogic

--- controller/api.controller.js
+++ controller/api.controller.js
@@ -4,17 +4,38 @@ const LeaderBoardModel = require("../models/board.model");
 const boardModel = new LeaderBoardModel();
 
 exports.getTopUsers = async (req, res) => {
+  /* specific user */
   const id = req.params.id;
-  const query = await UserModel.getIdAndValue();
+  const query = await UserModel.getUserInfo();
+  /* adds users */
   boardModel.addUsersForLeaderboard(query);
   if (!id) {
     const leaderboard = await boardModel.getTopUsers(100);
     res.json(leaderboard);
     return;
   }
+  /* get top user list */
   const leaderboard = await boardModel.getTopUsers(100);
-  const around = await boardModel.getArounds(id);
-  const concatted = leaderboard.concat(around);
+  const countryTopUsers = await UserModel.getTopUsers(leaderboard); //Promise
+  /* get countryAroundUsers */
+  const around = await boardModel.getArounds(id); //json
+  const countryAroundUsers = await UserModel.getTopUsers(around); //Promise
 
-  res.send(concatted);
+  Promise.all(countryTopUsers)
+    .then((topUsers) => {
+      const arr = leaderboard.map((user, index) => {
+        return Object.assign(user, topUsers[index]);
+      });
+      return arr;
+    })
+    .then((arr) => {
+      Promise.all(countryAroundUsers).then((aroundUsers) => {
+        const arrAround = aroundUsers.map((user, index) => {
+          return Object.assign(user, around[index]);
+        });
+        const resp = arr.concat(arrAround);
+        res.json(resp);
+      });
+    });
+  return;
 };
