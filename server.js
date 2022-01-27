d42981272d10dec7f2583f30cc6723bfab3d850b
ozgencdev <ozgencdev@gmail.com>
2022-01-28 02:46:12 +0300

    edit: changes scripts to node

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use("/play", playRouter);
 task.start();
 taskUpdateCache.start();
 
-app.listen(3000, function () {
-  console.log("Server listening port 3000");
+app.listen(process.env.PORT, function () {
+  console.log("Server listening");
 });
