a18d7e2e0715615ee72213a6b9c2db6018202460
ozgencdev <ozgencdev@gmail.com>
2022-01-28 13:10:42 +0300

    edit: test for play frontend rank color changes

---
+++ server.test.js
@@ -0,0 +1,58 @@
+const _ = require("lodash");
+const axios = require("axios");
+
+const arr = [
+  "tmaccart6a",
+  "tgopsell7o",
+  "sbarringer5g",
+  "rsauratgn",
+  "mchamberlenac",
+  "mcamocken9",
+  "eflippen8d",
+  "ecawoodb5",
+  "cmccumskay2u",
+  "bhawsonok",
+];
+
+describe("Play game", () => {
+  test("100 coins", async () => {
+    const random = _.random(0, arr.length - 1);
+    const response = await axios.post(
+      `https://panteon-backend.herokuapp.com/play/add/${arr[random]}/100`
+    );
+
+    expect(response.status).toBe(200);
+  });
+  test("200 coins", async () => {
+    const random = _.random(0, arr.length - 1);
+    const response = await axios.post(
+      `https://panteon-backend.herokuapp.com/play/add/${arr[random]}/200`
+    );
+
+    expect(response.status).toBe(200);
+  });
+  test("300 coins", async () => {
+    const random = _.random(0, arr.length - 1);
+    const response = await axios.post(
+      `https://panteon-backend.herokuapp.com/play/add/${arr[random]}/300`
+    );
+
+    expect(response.status).toBe(200);
+  });
+  test("100 coins", async () => {
+    const random = _.random(0, arr.length - 1);
+    const response = await axios.post(
+      `https://panteon-backend.herokuapp.com/play/add/${arr[random]}/100`
+    );
+    console.log(response);
+    expect(response.status).toBe(200);
+  });
+  test("200 coins", async () => {
+    const random = _.random(0, arr.length - 1);
+    const response = await axios.post(
+      `https://panteon-backend.herokuapp.com/play/add/${arr[random]}/200`
+    );
+
+    expect(response.status).toBe(200);
+  });
+});
